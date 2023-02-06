aaddeb143ae380c8d0fa20595f7743fa9c766e5a
Freddy Monroy <fredydeltoro@gmail.com>
2023-02-06 15:06:03 -0600

    FlatList horizontal

--- src/components/coinDetail/CoinDetailScreen.tsx
+++ src/components/coinDetail/CoinDetailScreen.tsx
@@ -1,9 +1,12 @@
-import {StyleSheet, Text, View, Image, SectionList} from 'react-native';
+import {StyleSheet, Text, View, Image, SectionList, FlatList} from 'react-native';
 import {useEffect, useState} from 'react';
-import colors from '../res/colors';
+import colors from '../../res/colors';
+import Http from '../../libs/http';
+import CoinMarketItem from './CoinMarketItem';
 
 const CoinDetailScreen = ({route, navigation}: any) => {
   const [coin, setCoin] = useState<any>({});
+  const [markets, setMarkets] = useState<any[]>([])
 
   const getSections = () => [
     {
@@ -20,16 +23,27 @@ const CoinDetailScreen = ({route, navigation}: any) => {
     },
   ];
 
-  useEffect(() => {
-    navigation.setOptions({title: route.params.coin.symbol});
-    setCoin(route.params.coin);
-  }, []);
-
   const getIcon = (name: string) =>
     name &&
     `https://c1.coinlore.com/img/25x25/${name
       .toLocaleLowerCase()
       .replace(' ', '-')}.png`;
+    
+  const getMarkets = async (coinId: string) => {
+    const res = await Http.instance.get(`https://api.coinlore.net/api/coin/markets/?id=${coinId}`)
+    setMarkets(res)
+  }
+
+  useEffect(() => {
+    setCoin(route.params.coin);
+  }, []);
+
+  useEffect(() => {
+    if (coin.symbol) {
+      navigation.setOptions({title: coin.symbol});
+      getMarkets(coin.id)
+    }
+  }, [coin])
 
   return (
     <View style={styles.container}>
@@ -37,7 +51,9 @@ const CoinDetailScreen = ({route, navigation}: any) => {
         <Image style={styles.img} source={{uri: getIcon(coin.name)}} />
         <Text style={styles.titleText}>{coin.name}</Text>
       </View>
-      <SectionList 
+
+      <SectionList
+        style={styles.section}
         sections={getSections()}
         keyExtractor={(item) =>  item}
         renderItem={({ item }) =>   (
@@ -51,6 +67,17 @@ const CoinDetailScreen = ({route, navigation}: any) => {
           </View>
         )}
       />
+
+      <View>
+        <Text style={styles.marketTitle}>Markets</Text>
+        <FlatList
+          data={markets}
+          horizontal={true}
+          renderItem={({ item }) => (
+            <CoinMarketItem item={item} />
+          )}
+        />
+      </View>
     </View>
   );
 };
@@ -75,6 +102,9 @@ const styles = StyleSheet.create({
     width: 25,
     height: 25,
   },
+  section: {
+    maxHeight: 220
+  },
   sectionHeader:{
     backgroundColor: "rgba(0, 0, 0, 0.2)",
     padding: 8
@@ -90,6 +120,12 @@ const styles = StyleSheet.create({
     color: colors.white,
     fontSize: 14,
     fontWeight: 'bold'
+  },
+  marketTitle: {
+    color: colors.white,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    marginLeft: 16
   }
 });
 
---
+++ src/components/coinDetail/CoinMarketItem.jsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import colors from '../../res/colors';
+
+const CoinMarketItem = ({ item }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.nameText}>{ item.name }</Text>
+      <Text style={styles.priceText}>{ item.price_usd }</Text>
+    </View>
+  )
+}
+
+export default CoinMarketItem
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    borderColor: colors.zircon,
+    borderWidth: 1,
+    padding: 16,
+    marginRight: 8,
+    alignItems: 'center',
+  },
+  nameText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  priceText: {
+    color: '#fff'
+  }
+})
