0e57e0e33dae4e084c5ebf1ece60e84c85449986
Freddy Monroy <fredydeltoro@gmail.com>
2023-02-07 18:24:50 -0600

    Storage lib

---
+++ src/libs/storaje.ts
@@ -0,0 +1,62 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+class Storage {
+  static instance = new Storage()
+
+  store = async (key: string, value: string) => {
+    try {
+      await AsyncStorage.setItem(key, value)
+      
+      return true
+    } catch (err) {
+      console.error("storage store error", err)
+
+      return false
+    }
+  }
+
+  get = async (key: string) => {
+    try {
+      await AsyncStorage.getItem(key)
+    } catch (err: any) {
+      console.error("storage get error", err)
+
+      throw Error(err)
+    }
+  }
+
+  getAllKeys =async () => {
+    try {
+      return await AsyncStorage.getAllKeys()
+      
+    } catch (err: any) {
+      console.error("storage get error", err)
+
+      throw Error(err)
+    }
+  }
+
+  multiGet =async (keys: string[]) => {
+    try {
+      
+    } catch (err: any) {
+      console.error("storage get error", err)
+
+      throw Error(err)
+    }
+  }
+
+  remove = async (key: string) => {
+    try {
+      await AsyncStorage.removeItem(key)
+      
+      return true
+    } catch (err) {
+      console.error("storage get error", err)
+
+      return false
+    }
+  }
+}
+
+export default Storage
